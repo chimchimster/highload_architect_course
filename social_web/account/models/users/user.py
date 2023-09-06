fcbcb3fd7ca195f8cb4c14f2784a28e39ee6504a
Chimchimster <jorddyishere@gmail.com>
2023-09-06 19:08:01 +0600

    find bug with cursor

--- social_web/account/models/users/user.py
+++ social_web/account/models/users/user.py
@@ -1,30 +1,23 @@
 import asyncio
 import datetime
-from typing import Callable
 
-from highload_architect_course.social_web.account.models.utils import UserModel, QueryTemplate, DatabaseHandler
-
-
+from highload_architect_course.social_web.account.models.utils import UserModel, QueryTemplate, DatabaseHandlerDecorator
+from highload_architect_course.social_web.account.models.utils.db_handler import DatabaseHandler
 
 
 class User(UserModel, QueryTemplate):
     """ Класс-представление для миграции объекта пользователя в СУБД. """
 
 
-    __available_methods__ = [
-        'create_user',
-    ]
-
-    def __init__(self, first_name, last_name, password, email, date_of_birth, handler: DatabaseHandler, table='users'):
+    def __init__(self, first_name, last_name, password, email, date_of_birth, table='users'):
         super().__init__(first_name, last_name, password, email, date_of_birth)
         self._table = table
-        self._handler = handler
-
 
     @property
     def table_of_users(self) -> str:
         return self._table
 
+    @DatabaseHandlerDecorator()
     async def create_user(self) -> str:
         query = self.CREATE_USER_TEMPLATE.format(
             table=self.table_of_users,
@@ -36,24 +29,20 @@ class User(UserModel, QueryTemplate):
         )
         return query
 
-    async def execute(self, func: Callable):
-        for method_name in self.__available_methods__:
-            if method_name == func.__name__:
-                await self._handler.execute_operation(func)
+
 
 async def main():
     handler = DatabaseHandler()
+
     u = User(
         'artem',
         'kasyan',
         'password1',
         'email@email.com',
         datetime.datetime(1994, 5, 6),
-        handler,
     )
 
-    await u.execute(u.create_user)
-
+    await handler.execute(u.create_user)
 
 if __name__ == '__main__':
     asyncio.run(main())
--- social_web/account/models/utils/db_handler.py
+++ social_web/account/models/utils/db_handler.py
@@ -9,9 +9,16 @@ from typing import Callable, Any
 
 
 class DatabaseHandler:
+
+    __available_methods__ = [
+        'create_user',
+        'wrapper',
+    ]
+
+
     def __init__(self):
         load_dotenv()
-        self._db_name = os.environ.get('pg_host')
+        self._db_name = os.environ.get('pg_db_name')
         self._user = os.environ.get('pg_user')
         self._password = os.environ.get('pg_password')
         self._host = os.environ.get('pg_host')
@@ -37,24 +44,38 @@ class DatabaseHandler:
     def dsn(self) -> str:
         return self._dsn
 
-    async def execute_operation(self, func: Callable) -> Any:
+    async def __handle_operation(self, func: Callable) -> Any:
+        """ Метод-обертка позволяющий выполнять операции в PostgreSQL. """
+
         async with aiopg.create_pool(self.dsn, timeout=3.0, minsize=5, maxsize=10) as pool:
             async with pool.acquire() as connection:
                 async with connection.cursor() as cursor:
                     @wraps(func)
                     async def wrapper(*args, **kwargs):
                         try:
-                            await cursor.execute('BEGIN')
                             query = await func(*args, **kwargs)
-
                             await cursor.execute(query)
-                            await cursor.execute('COMMIT')
                         except asyncio.TimeoutError as exc:
-                            await cursor.execute('ROLLBACK')
                             raise exc
                         except psycopg2.Error as exc:
-                            await cursor.execute('ROLLBACK')
                             raise exc
 
                     return wrapper
 
+    async def execute (self, func: Callable, *args, **kwargs) -> Any:
+        for method_name in self.__available_methods__:
+            if method_name == func.__name__:
+                wrapped_result = await self.__handle_operation(func)
+                return await wrapped_result(*args, **kwargs)
+
+        raise ValueError(f'Метод {func.__name__} не доступен в данном классе!')
+
+
+class DatabaseHandlerDecorator:
+    def __init__(self):
+        self.handler = DatabaseHandler()
+
+    def __call__(self, func):
+        async def wrapper(instance, *args, **kwargs):
+            await self.handler.execute(func, instance, *args, **kwargs)
+        return wrapper
